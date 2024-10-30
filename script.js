57ee1adf7ac3c20264aaec0b2bc8e187f6b9bb2f
david <davidsoliss123@gmail.com>
2024-10-29 23:35:27 -0500

    Variables booleanas

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // varianle tipo String
 let myName = "Cristian"
 console.log(myName)
+
+// Boolean
+let isDay = true
+
+if (isDay == true) {
+    console.log("Es de día.");
+} else {
+    console.log("Es de noche.");
+}
