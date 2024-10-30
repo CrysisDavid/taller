a19a04db6e85ede9bf3782d7c5fb4ed87e71f339
david <davidsoliss123@gmail.com>
2024-10-29 23:40:41 -0500

    Variable entero

--- script.js
+++ script.js
@@ -3,10 +3,20 @@ let myName = "Cristian"
 console.log(myName)
 
 // Boolean
-let isDay = true
+let isDay = false
 
 if (isDay == true) {
     console.log("Es de día.");
 } else {
     console.log("Es de noche.");
 }
+
+// Numeros enteros
+
+let numero1 = 50;
+let numero2 = 60;
+
+
+let suma = numero1 + numero2;
+console.log("La suma es:", suma);
+
