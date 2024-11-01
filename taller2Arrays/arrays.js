febcd3874dfbc275470d676b4dda1beaa431466a
david <davidsoliss123@gmail.com>
2024-11-01 12:49:01 -0500

    modified the variable sumatorio and added more comments

--- taller2Arrays/arrays.js
+++ taller2Arrays/arrays.js
@@ -1,13 +1,20 @@
 let altura = [1.75, 1.78, 1.79, 1.82, 1.69]
-console.log(altura)
-let sumatorio = altura[0] + altura[1] + altura[2] + altura[3] + altura[4]
-let promedio = sumatorio / altura.length
+console.log(altura + "\n")
 
+let sumatorio = 0;
+for (let persona = 0; persona < altura.length; persona++) {
+    // sumo todas las edades del array y lo guardo en la variable sumatorio
+    sumatorio += altura[persona];
+}
+console.log(`la suma de la edades es ${sumatorio}`)
+
+
+let promedio = sumatorio / altura.length
 
 let masAltas = 0;
 let masBajas = 0;
 
-for (let persona = 0; persona < altura.length; 1) {
+for (let persona = 0; persona < altura.length; persona++) {
 
     if (altura[persona] > promedio) {
         masAltas++;
@@ -15,7 +22,6 @@ for (let persona = 0; persona < altura.length; 1) {
     } else if (altura[persona] < promedio) {
         masBajas++;
     }
-    persona++;
 
 }
 
@@ -29,21 +35,25 @@ console.log(`
 let fruits = ["manzana", "pera", "platano", "cereza"]
 //agrega un elemento al final del arreglo
 fruits.push("mango")
-console.log(fruits)
+console.log(fruits, "se agregó el mando al final del arreglo")
 
 //agrega un elemento al inicio del arreglo
 fruits.unshift("fresa")
-console.log(fruits)
+console.log(fruits, "se agrega la fresa al principio del arrgelo")
 
 //elimina el último elemento del arreglo
 fruits.pop()
-console.log(fruits)
+console.log(fruits, "se eliminó el mango del arreglo")
 
 //elimina el primer elemento del arreglo
 fruits.shift()
-console.log(fruits)
+console.log(fruits, "se eliminó la fresa del arreglo\n")
 
 
-console.log(`el platano esta en la posicion ${fruits.indexOf("platano")}`)
-fruits = Object.entries(fruits).map(([key, value]) => ({ posición: key, valor: value }))
-console.log(fruits)
+console.log(`el platano esta en la posicion ${fruits.indexOf("platano")}\n`)
+
+
+// modiqué la estructura del array para que este ahora sea un array de objectos indicando la posicion y el en el array y su valor
+fruits = Object.entries(fruits).map(([key, value]) => ({ index: key, valor: value }))
+
+console.log(fruits) // aquí se comprueba el estado del arreglo
