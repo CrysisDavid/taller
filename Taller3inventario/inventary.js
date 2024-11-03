89fbcd9af59e684e0126ff2aaee72f405b5fd00a
david <davidsoliss123@gmail.com>
2024-11-03 02:33:16 -0500

    a menu was added, method verProductos was added and openStore function was created

--- Taller3inventario/inventary.js
+++ Taller3inventario/inventary.js
@@ -9,18 +9,19 @@ const prompt = require('prompt-sync')()
  */
 
 let tineda = {
+    nombre: "La tienda de Remington",
     products: [ // products es un atributo del objecto tienda, y este a la vez es un arreglo de objectos que contiene la info de los productos
         {
-            id: "1",
+            id: 1,
             name: "papa",
-            price: "1.99",
-            cantidad: "10"
+            price: 1.99,
+            cantidad: 10,
         },
         {
-            id: "2",
+            id: 2,
             name: "cebolla",
-            price: "0.99",
-            cantidad: "20"
+            price: 0.99,
+            cantidad: 20
         },
     ],
 
@@ -36,8 +37,8 @@ let tineda = {
 
             let products = { // a este objecto se le actualizarán sus atributos durante la ejecución
                 id: undefined,
-                nombre: undefined,
-                precio: undefined,
+                name: undefined,
+                price: undefined,
                 cantidad: undefined
             }
 
@@ -45,7 +46,7 @@ let tineda = {
             while (tineda.products.find((p) => p.id == id)) { // el id debe ser unico ya que este nos permite diferencias e identificar productos
                 id = prompt("el id debe ser unico intenta con otro: ")// si el id ingresado por el user ya esta en el inventario entra a un ciclo
             };
-            products.id = id; // agregar el id al objecto 
+            products.id = parseInt(id); // agregar el id al objecto 
 
             /*
             * al igual que con el id para el resto de atributos se hará lo mismo, se crea la variable y la variable se le asigna al atributo
@@ -53,21 +54,21 @@ let tineda = {
             */
 
             let nombre = prompt("nombre del producto: ")
-            products.nombre = nombre;
+            products.name = nombre;
 
             let precio = prompt("precio: ")
-            products.precio = precio;
+            products.price = parseInt(precio);
 
             let cantidad = prompt("cantidad: ")
-            products.cantidad = cantidad;
+            products.cantidad = parseInt(cantidad);
 
             tineda.products.push(products)
 
             addMore = prompt("¿deseas agregar otro producto? (si/no)") // se reasigna la variable addMore
 
         } while (addMore.toLowerCase() == "si") // si el user desea agregar más se repite todo el proceso hasta que el user desea finalizar
+        openStore()
 
-        console.log(JSON.stringify(tineda.products, null, 4))// JSON.stringify() es un parser que permiter parsear objectos a json
 
     },
 
@@ -87,31 +88,65 @@ let tineda = {
 
                 let updatedProduct = filteredProduct[0] // se crea el objecto con el valor de filteredProducts
 
-                updatedProduct.cantidad = cantidadToUpdate // y se accede a su atributo cantidad para actualizarlo con la cantidad indicada por el user, ahora el objecto estará actualizado con la cantidad
+                updatedProduct.cantidad = parseInt(cantidadToUpdate) // y se accede a su atributo cantidad para actualizarlo con la cantidad indicada por el user, ahora el objecto estará actualizado con la cantidad
 
                 tineda.products = tineda.products.filter((tienda) => tienda.id != idToUpdate) // se filtra y a su se reasigna el arreglo products con todos los valores que no coincidan con el id indicado, para asi poder elimiar el valor antiguo
 
                 tineda.products.push(updatedProduct)// finalmente se agregan el objecto actualizado a ese arreglo
-
-                console.log(`cantidad actualizada\n${JSON.stringify(updatedProduct, null, 4)}`)// finalmente  se muestra el objecto actualizado
+                console.log("cantidad actualizada\n")
+                console.table(updatedProduct)// finalmente  se muestra el objecto actualizado
 
             } else console.log("el producto no existe")
 
             retry = prompt("¿actualizar otro producto? si/no: ")
         }
         while (retry.toLowerCase() == "si")
+        openStore()
+    },
+
+
+    verProductos: () => {
+        console.table(tineda.products)
+        openStore()
     }
+};
+
+const Menu = () => {
+    console.log(`
+        Bienvenido a la tienda de ${tineda.nombre}
+        \n
+        1. Agregar producto
+        2. Actualizar producto
+        3. Ver productos
+        4. Salir
+        `)
 }
 
 // 
 
+const openStore = () => {
+    Menu()
+    let option = prompt("")
+    switch (option) {
+        case "1":
+            tineda.agregar()// llamada al metodo agregar()
+            break
+        case "2":
+            tineda.updateQuantity()// llamada al metodo updateQuantity()
+            break
+        case "3":
+            tineda.verProductos()// llamada al metodo verProductos()
+            break
+        case "4":
+            console.log("adiós")
+            break
+        default:
+            console.log("opcion invalida")
+            break
 
-tineda.agregar()// llamada al metodo agregar()
+    }
+
+};
 
 
-
-
-tineda.updateQuantity()// llamada al metodo updateQuantity()
-
-console.log(`\ninventario actual\n`, tineda.products)
-
+openStore()
